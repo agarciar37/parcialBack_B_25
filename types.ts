d59ef37b589fff7f8f79b0005b21e62f283f8d1d
agarciar37 <agarciar37@alumnos.nebrija.es>
2025-06-23 17:30:05 +0200

    Primer commit

---
+++ types.ts
@@ -0,0 +1,25 @@
+import { ObjectId, OptionalId } from "mongodb"
+
+export type BookModel = OptionalId<{
+    title: string;
+    authors: ObjectId[];
+    numberOfCopies: number;
+}>
+
+export type AuthorModel = OptionalId<{
+    name: string;
+    biography: string;
+}>
+
+export type Author = {
+  id: string;
+  name: string;
+  biography: string;
+};
+
+export type Book = {
+  id: string;
+  title: string;
+  authors: Partial<Author>[];
+  numberOfCopies: number;
+};
